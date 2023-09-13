04b119b431d66ded1ce8f4c9c821c64bf06e6504
Rupam Pidurkar <rupampidurkar57@gmail.com>
2023-09-14 02:44:09 +0530

    dwd

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import img1 from "../assets/team.webp";
 import img2 from "../assets/values.webp";
+import img5 from "../assets/handbook.webp";
 import img3 from "../assets/security.png";
 import img4 from "../assets/bright.png";
+import img6 from "../assets/person.png";
+import img7 from "../assets/telephone.png";
 import Testimonials from "../components/Testimonials";
 import Something from "../components/Something";
 const About = () => {
@@ -31,7 +34,7 @@ const About = () => {
 
       {/****************Vision****************** */}
 
-      <div className="flex flex-col md:flex-row gap-6 items-center px-4 md:px-28 py-12 ">
+      <div className="flex flex-col md:flex-row gap-14 items-center px-4 md:px-28 py-12 ">
         <div className="flex flex-col justify-between gap-10">
           <p className="bg-gradient-to-r from-[#333139] to-[#27232C] w-fit px-6 py-2 rounded-tl-full  rounded-r-full ">
             Our vision
@@ -81,6 +84,60 @@ const About = () => {
 
         <div>
           <img src={img2} alt="" className="rounded-3xl" />
+        </div>
+      </div>
+
+      {/**************************************Handbook**************************************************** */}
+
+      <div className="flex flex-col md:flex-row gap-14 items-center px-4 md:px-28 py-12 ">
+        <div>
+          <img src={img5} alt="" className="rounded-3xl" />
+        </div>
+        <div className="flex flex-col justify-between gap-10">
+          <p className="bg-gradient-to-r from-[#333139] to-[#27232C] w-fit px-6 py-2 rounded-tl-full  rounded-r-full ">
+            Our handbook
+          </p>
+          <p className="md:text-6xl text-4xl font-bold  ">
+            How we work and collaborate with our clients
+          </p>
+          <p className="text-2xl text-[#C2C7DC]">
+            Our work philosophy revolves around active collaboration and open
+            dialogue. We engage with clients every step of the way, ensuring
+            their vision is at the heart of our designs and strategies.
+          </p>
+
+          {/***************************** */}
+
+          <span className="flex flex-col md:flex-row">
+            <div>
+              <span className="flex gap-4 items-center">
+                <img
+                  src={img6}
+                  alt=""
+                  className="h-12 p-2 rounded-full bg-gradient-to-r from-[#333139] to-[#27232C]"
+                />
+                <p className="text-xl font-bold">User focused</p>
+              </span>
+              <p className="text-xl text-[#C2C7DC]">
+                Trust is key; we uphold honesty. Every step is communicated for
+                clarity.
+              </p>
+            </div>
+            <div>
+              <span className="flex  gap-4 items-center">
+                <img
+                  src={img7}
+                  alt=""
+                  className="h-12 p-2 rounded-full bg-gradient-to-r from-[#333139] to-[#27232C]"
+                />
+                <p className="text-xl font-bold">Clear communication</p>
+              </span>
+              <p className="text-xl text-[#C2C7DC]">
+                Our focus: user-centric, engaging, and valuable products with
+                purpose.
+              </p>
+            </div>
+          </span>
         </div>
       </div>
       <Testimonials />
